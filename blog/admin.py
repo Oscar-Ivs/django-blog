b24c30f8323623f316935c537a6d29b8cefad763
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-07-02 10:08:16 +0100

    Register PostComment model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostComment
+
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display = ('sno', 'user', 'post', 'created_at', 'parent_comment')
+    search_fields = ('comment', 'user__username', 'post__title')
+    list_filter = ('created_at',)
 
 admin.site.register(Post)
+admin.site.register(PostComment, PostCommentAdmin)
