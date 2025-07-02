d32ff9562ece4d2856adf2c3e4ba134f68358fdd
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-07-02 10:38:16 +0100

    Correct admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'post', 'author', 'approved', 'created_on')
-    search_fields = ('body', 'author__username', 'post__title')
-    list_filter = ('approved', 'created_on')
-
+# Register your models here.
 admin.site.register(Post)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Comment)
