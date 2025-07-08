ea921ec0bab0128ed759ace4d2867dae97b67de6
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-07-08 12:25:09 +0100

    Added About page

--- about/views.py
+++ about/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import About
+
+def about_view(request):
+    about = get_object_or_404(About)
+    return render(request, "about/about.html", {"about": about})
