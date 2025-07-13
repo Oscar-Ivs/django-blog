3f11c56af463953b363cf0a3fd4f4c0bcc142214
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-07-13 11:19:21 +0100

    Fixed API key

--- env.py
+++ env.py
@@ -2,5 +2,7 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_cbkgQzLh9mX3@ep-flat-dream-a2ly1m2f.eu-central-1.aws.neon.tech/cod_bony_enter_520594"
 )
+import os
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dcyyzcqtj")
+    "CLOUDINARY_URL", "cloudinary://353585423877585:your_actual_api_secret@dcyyzcqtj"
+)
